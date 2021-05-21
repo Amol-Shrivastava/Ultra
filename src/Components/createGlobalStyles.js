19795cbda530083a7166eb76e5437334c59ff0ea
Amol <amolshrivastava448@gmail.com>
2021-05-21 23:22:47 +0530

    Font Link added

--- src/Components/createGlobalStyles.js
+++ src/Components/createGlobalStyles.js
@@ -9,7 +9,6 @@ export const GlobalStyle = createGlobalStyle`
     }
 
     body{
-        @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700&display=swap');
         font-family: 'Inter', sans-serif !important;
         font-size: 62.5%;
         overflow-x:hidden;
